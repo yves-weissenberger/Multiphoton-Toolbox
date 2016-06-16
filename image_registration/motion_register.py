65151cc27475f5b65548f43290fd19f6a9713627
yves-weissenberger <yvesweissenberger@gmail.com>
2016-06-16 21:06:54 +0100

    overhaul of motion registration pipeline

--- image_registration/motion_register.py
+++ image_registration/motion_register.py
@@ -11,10 +11,11 @@ def copy_ROIs(HDF_File):
 
     return None
 
-def register_dayData(HDF_File,session_ID):
+def register_dayData(HDF_File,session_ID,inRAM=True):
 
     HDF_PATH = str(HDF_File.filename)
-
+    global inRAM_flag
+    inRAM_flag=inRAM
 
     if 'registered_data' not in HDF_File[session_ID].keys():
         HDF_File[session_ID].create_group('registered_data')
@@ -41,36 +42,50 @@ def register_dayData(HDF_File,session_ID):
             #raw_file = np.array(HDF_File[session_ID]['raw_data'][file_key])
             raw_file = HDF_File[session_ID]['raw_data'][file_key]
             #print 'file open %ss' %(time.time() - st)
-            #________________________________________________________________
             st = time.time()
-            regIms, shifts, tot_shifts = motion_register(raw_file,2)
-            print 'Motion Register Duration %ss' %(time.time() - st)
+
+            if not inRAM:
+                global regFile
+                regFile = HDF_File[session_ID]['registered_data'].create_dataset(name=file_key,
+                                                                                 shape=raw_file.shape,
+                                                                                 chunks=(10,512,512),dtype='int16')
+                st = time.time()
+                shifts, tot_shifts = motion_register(raw_file,2,inRAM)
+                regFile.attrs['mean_image'] = np.mean(regFile,axis=0)
+
+            else:
             #________________________________________________________________
-            st = time.time()
-            regFile = HDF_File[session_ID]['registered_data'].create_dataset(name=file_key,
-                          	                                                 data=regIms.astype('int16'),
-                 	                                                         chunks=(10,512,512),dtype='int16')
+                regIms, shifts, tot_shifts = motion_register(raw_file,2,inRAM)
 
-            HDF_File.close()
-            HDF_File = h5py.File(HDF_PATH,'a',libver='latest')
-            regFile =  HDF_File[session_ID]['registered_data'][file_key]
-            raw_file = HDF_File[session_ID]['raw_data'][file_key]
-            #print shifts
+                print 'Motion Register Duration %ss' %(time.time() - st)
+                #________________________________________________________________
+                st = time.time()
+                regFile = HDF_File[session_ID]['registered_data'].create_dataset(name=file_key,
+                              	                                                 data=regIms.astype('int16'),
+                     	                                                         chunks=(10,512,512),dtype='int16')
+                regFile.attrs['mean_image'] = np.mean(regIms.astype('int16'),axis=0)
 
-            #print 'success'
+
+            
+
             regFile.attrs['shifts'] = shifts
-            regFile.attrs['mean_image'] = np.mean(regIms.astype('int16'),axis=0)
             regFile.attrs['tot_shifts'] = tot_shifts
             for key,value in raw_file.attrs.iteritems():
                     if (key=='trigger_DM' or key=='ROI_centres' or key=='ROI_patches'):
                         regFile.attrs[key] = value
                     else:
             	       regFile.attrs[key] = str(value)
+
+            build_registration_log(regFile)
+
+            HDF_File.close()
+
             print 'file write in ram %ss' %(time.time() - st)
-            
+            HDF_File = h5py.File(HDF_PATH,'a',libver='latest')
+
+
         except IOError:
             print '!!!!!!!!!!!!!!!!!!! WARNING !!!!!!!!!!!!!!!! \n %s could not be loaded. Skipping \n !!!!!!!!!!!!!!!!!!! WARNING !!!!!!!!!!!!!!!!' %file_key
-        build_registration_log(regFile)
         HDF_File.close()
 
             #________________________________________________________________
@@ -94,9 +109,11 @@ def build_registration_log(areaFile):
 
     return None
 
-def motion_register(imArray,maxIter=5,Crop=True):
+def motion_register(imArray,maxIter=5,Crop=True,inRAM=True):
     
-    pool = Pool(5)
+    if inRAM:
+        pool = Pool(5)
+
     global crop
     crop = Crop
     imArray = imArray
@@ -108,13 +125,24 @@ def motion_register(imArray,maxIter=5,Crop=True):
 
     converged = False
     iteration = 0
-    imgList= [imArray[i] for i in range(imArray.shape[0])]
+    imgList= [(i,imArray[i]) for i in range(imArray.shape[0])]
     tot_shift = np.zeros([imArray.shape[0],2])
+    print tot_shift.shape
     while not converged:
         strt = time.time()
-        temp = pool.map(register_image,imgList)
-        imgList = [i[1] for i in temp]
-        shifts = np.array([i[0] for i in temp])
+
+        if not inRAM:
+            temp = []
+            for entry in imgList:
+                temp.append(register_image(entry))
+                shifts = np.array(temp)
+            print shifts.shape
+        else:
+            temp = pool.map(register_image,imgList)
+            print inRAM_flag
+            imgList = np.array([i[1] for i in temp])
+            shifts = np.array([i[0] for i in temp])
+
         tot_shift += shifts
         iteration += 1
         mean_shift = np.mean(np.abs(shifts))
@@ -127,15 +155,21 @@ def motion_register(imArray,maxIter=5,Crop=True):
             print 'Convergence not-optimal'
             converged = True
 
-        if crop==True:
-            refIm = np.mean(imgList[:50],axis=0)[128:-128,128:-128]
-        else:
+        if inRAM:
             refIm = np.mean(imgList[:50],axis=0)
+        else:
+            refIm = np.mean(regFile[:50],axis=0)
+        if crop==True:
+            refIm = refIm[128:-128,128:-128]
 
-    return np.array(imgList), shifts, tot_shift
+    if not inRAM:
+        return shifts, tot_shift
+    else:
+        return np.array(imgList), shifts, tot_shift
 
-def register_image(image):
-
+def register_image(inp):
+    image_idx = inp[0]
+    image = inp[1]
     if crop==True:
         shift, _, _ = register_translation(refIm,image[128:-128,128:-128],upsample_factor=10)
     else:
@@ -145,4 +179,10 @@ def register_image(image):
         regIm =  np.fft.ifftn(fourier_shift(np.fft.fftn(image), shift)).real.astype('int16')
     else:
         regIm = image.astype('int16')
-    return [shift,regIm]
+
+    #added for reduce memory test
+    if not inRAM_flag:    
+        regFile[image_idx] = regIm
+        return shift
+    else:
+        return [shift,regIm]
