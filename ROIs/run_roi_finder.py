6f5431b919864f94aaa1d07ee6b3e442cd392f5c
yves-weissenberger <yvesweissenberger@gmail.com>
2018-05-09 13:50:18 +0100

    continued argparse integration

--- ROIs/run_roi_finder.py
+++ ROIs/run_roi_finder.py
@@ -1,3 +1,7 @@
+#!/home/yves/anaconda2/bin/python
+
+
+
 from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,6 +14,7 @@ import pickle
 import sys
 from skimage.exposure import equalize_adapthist
 import skimage
+import argparse
 
 def findpath():
     cDir = os.path.dirname(os.path.realpath(__file__))
@@ -33,14 +38,54 @@ import twoptb as MP
 
 if __name__ =="__main__":
 
-    rfC_fit,rad = pickle.load(open(sys.argv[1]))
-    hdfF11 = h5py.File(sys.argv[2])
+    class_path = os.path.join(os.path.split(os.path.split(os.path.realpath(__file__))[0])[0],'classifiers')
+
+    parser = argparse.ArgumentParser(description="""Automatically find ROIs based on a mean image example usage is:
+
+python run_roi_finder -sess 0 -ded 3 2 3 /path/to/hdf5.py /path/to/classifier
+
+Optional arguments, i.e. those with a hyphen preceding may be omitted note that here
+0 refers to sess and 3 2 3 refers to ded
+=============================================================================""",
+                                                    formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument("hdfPath", type=str,
+                    help="Full path to HDF5 file to open and search for ROIs in")
+
+    parser.add_argument("classifier", type=str,
+                    help="Full path to classifer")
+    
+    parser.add_argument("-sess",type=int,dest='sess',default=1,
+                    help="Session to run automatic ROI finding on")
+
+    parser.add_argument("-ded" ,type=int,nargs=3,dest='ded',
+                help=("Parameters for morphological operations for drawing ROIs. Required if zoom level is not 1 or 2. ded is short for dilation erosion dilation, which are run morphological operations"))
+
+
+    parser.add_argument("-thresh", type=float, dest='thresh', default=.925, help="Threshold recommended range 0.8-.98 the higher the threshold the harder the inlcusion threshold")
+
+    args = parser.parse_args()
+
+    rfC_fit,rad = pickle.load(open(args.classifier))
+    hdfF11 = h5py.File(args.hdfPath)
 
     #ded = [5,4,5] #for zoom 2
-    bhdf11 =  hdfF11[hdfF11.keys()[0]][ u'registered_data']
-    areas =bhdf11.keys()
-    meanIm11 = bhdf11[areas[-1]].attrs['mean_image']
 
+    areaIx = args.sess
+    bhdf11 =  hdfF11[hdfF11.keys()[0]][ u'registered_data']
+    areas = bhdf11.keys()
+    print "extracting from %s" %(areas[areaIx])
+    meanIm11 = bhdf11[areas[areaIx]].attrs['mean_image']
+
+    gInfoP = os.path.join(os.path.split(args.hdfPath)[0],'GRABinfos',os.path.split(bhdf11[areas[areaIx]].attrs['GRABinfo'])[-1])
+    zoom = pickle.load(open(gInfoP,'r'))['scanZoomFactor']
+
+    if zoom==2:
+        ded = [5,4,5]
+    elif zoom==1:
+        ded = [3,2,3]
+    else:
+        ded = args.ded
     meanIm11 = equalize_adapthist(meanIm11/np.max(meanIm11),clip_limit=0.005)
 
 
@@ -59,7 +104,7 @@ if __name__ =="__main__":
             ixs_sets.append([i,j])
 
 
-    labels_pred = rfC_fit.predict_proba(np.array([i.flatten() for i in ims]))>.975
+    labels_pred = rfC_fit.predict_proba(np.array([i.flatten() for i in ims]))>args.thresh
     bouton_pred_ixs = np.where(labels_pred==1)[0]
 
     mask = np.zeros([512,512])
@@ -67,8 +112,8 @@ if __name__ =="__main__":
     for i in bouton_pred_ixs:
         mask[ixs_sets[i][0],ixs_sets[i][1]] = 1
 
-    mask = morphology.dilation(mask,morphology.disk(5))
-    mask2 = morphology.erosion(mask,morphology.disk(4))
+    mask = morphology.dilation(mask,morphology.disk(ded[0]))
+    mask2 = morphology.erosion(mask,morphology.disk(ded[1]))
 
     label_im = skimage.morphology.label(mask2)
 
@@ -96,7 +141,7 @@ if __name__ =="__main__":
 
 
 
-        mask_temp = morphology.dilation(mask_temp,morphology.disk(4))
+        mask_temp = morphology.dilation(mask_temp,morphology.disk(ded[2]))
 
         nIxs2 = np.where(mask_temp>0)
 
@@ -142,17 +187,17 @@ if __name__ =="__main__":
 
     if yn=='y':
 
-        fName = bhdf11[areas[-1]].name[1:].replace('/','-') + '_ROIs.p'
+        fName = bhdf11[areas[areaIx]].name[1:].replace('/','-') + '_ROIs.p'
         #print os.path.join(self.Folder,fName)
-        Folder = os.path.split(os.path.abspath(bhdf11[areas[-1]].file.filename))[0]
+        Folder = os.path.split(os.path.abspath(bhdf11[areas[areaIx]].file.filename))[0]
 
         FLOC = os.path.join(Folder,'ROIs',fName)
         with open(FLOC,'wb') as f:
             pickle.dump(ROI_attrs,f)
 
-        bhdf11[areas[-1]].attrs['ROI_dataLoc'] = FLOC
+        bhdf11[areas[areaIx]].attrs['ROI_dataLoc'] = FLOC
         hdfF11.close()
-        print 'saved ROIs for session %s' %(areas[-1])
+        print 'saved ROIs for session %s' %(areas[areaIx])
     else:
         print "done!"
 
